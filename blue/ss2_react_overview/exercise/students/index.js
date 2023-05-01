2a3fdae463220aa8d57088b62a1d1a7e5a155cd7
thuydiem1702 <thuydiem17022001@gmail.com>
2023-05-01 16:25:41 +0700

    update

---
+++ blue/ss2_react_overview/exercise/students/index.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+const students = [
+  {
+    company: 'Alfreds Futterkiste',
+    contact: 'Maria Anders',
+    country: 'Germany'
+  },
+  {
+    company: 'Centro comercial Moctezuma',
+    contact: 'Francisco Chang',
+    country: 'Mexico'
+  },
+  {
+    company: 'Ernst Handel',
+    contact: 'Roland Mendel',
+    country: 'Austria'
+  },
+  {
+    company: 'Island Trading',
+    contact: 'Helen Bennett',
+    country: 'UK'
+  },
+  {
+    company: 'Laughing Bacchus Winecellars',
+    contact: 'Yoshi Tannamuri',
+    country: 'Canada'
+  },
+  {
+    company: 'Magazzini Alimentari Riuniti',
+    contact: 'Giovanni Rovelli',
+    country: 'Italy'
+  }
+]
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+
+  <table>
+
+    <thead>
+      {/* <h1>Students</h1> */}
+      <tr>
+        <th>No.</th>
+        <th>Company</th>
+        <th>Contact</th>
+        <th>Country</th>
+      </tr>
+    </thead>
+    <tbody>
+      {
+        students.map((item, index) =>
+          <tr>
+            <td>{index + 1}</td>
+            <td>{item.company}</td>
+            <td>{item.contact}</td>
+            <td>{item.country}</td>
+          </tr>
+        )
+      }
+    </tbody>
+  </table>
+
+);
+
---
+++ blue/ss2_react_overview/practice/your_name/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const name = "Dĩm Xinhhh"
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <h1 style={{ textAlign: "center" }}>
+    {name}
+  </h1>
+);
+
